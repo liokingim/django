ded03d46022397692c632822b38793a98d250646
liokingim <liokingim@daum.net>
2019-06-05 02:58:22 +0900

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6!&@jf2z34^n=lxv%3in-2=w7n&g@ney!b4a(wdh0a!b0-qqcw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
